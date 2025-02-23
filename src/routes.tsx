0104958298a0a5e0113a7211054b9ccc3b4650ca
Juan Miguel Rojas Noriega <juan.rojas@pragma.com.co>
2025-02-23 14:52:42 -0500

    Se realiza la base del sidebar

---
+++ src/routes.tsx
@@ -0,0 +1,8 @@
+import HomeIcon from './UI-Components/assets/icons/home.svg'
+export const appRoutes = [
+  {
+    path: '/h',
+    icon: HomeIcon,
+    name: 'Home' // Opcional, podría usarse para tooltips
+  }
+]
